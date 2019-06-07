f2d7ec6d7605b6f0aa22f98a0226acb37a0c2688
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-07 17:19:34 +0800

    new

---
+++ github/github.py
@@ -0,0 +1,17 @@
+import re
+from discord.ext import commands
+
+
+class Github(commands.Cog):
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        match = re.match(r'plugins#(\d+)', message.content)
+        if match:
+            issue_num = match.group(1)
+            await message.channel.send(f'https://github.com/snaildos/modmail-plugins/issues/{issue_num}')
+
+
+def setup(bot):
+    bot.add_cog(Github())
