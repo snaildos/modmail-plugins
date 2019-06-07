ab1185ba7cbc86f9f09e4102de129d0b2aa4d7d2
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-07 20:12:33 +0800

    Update github.py

--- github/github.py
+++ github/github.py
@@ -16,7 +16,7 @@ class Github(commands.Cog):
 
    @commands.Cog.listener()
     async def on_message(self, message):
-        match = re.match(r'hdr#(\d+)', message.content)
+        match = re.match(r'harddisk#(\d+)', message.content)
         if match:
             issue_num = match.group(1)
             await message.channel.send(f'https://github.com/snaildos/SnailDOS-Hard-Disk-Repair/issues/{issue_num}')
