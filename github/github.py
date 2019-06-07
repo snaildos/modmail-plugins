6d577f30f76cf9baa540d82f560019a0f4293530
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-07 18:21:00 +0800

    New version

--- github/github.py
+++ github/github.py
@@ -13,5 +13,14 @@ class Github(commands.Cog):
             await message.channel.send(f'https://github.com/snaildos/modmail-plugins/issues/{issue_num}')
 
 
+
+   @commands.Cog.listener()
+    async def on_message(self, message):
+        match = re.match(r'hdr#(\d+)', message.content)
+        if match:
+            issue_num = match.group(1)
+            await message.channel.send(f'https://github.com/snaildos/SnailDOS-Hard-Disk-Repair/issues/{issue_num}')
+
+
 def setup(bot):
     bot.add_cog(Github())
