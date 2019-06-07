c62ab83ce91cd4a04f2a0328729e7458610e0c95
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-07 20:50:24 +0800

    Update github.py

--- github/github.py
+++ github/github.py
@@ -1,6 +1,8 @@
 import re
 from discord.ext import commands
 
+def __init__(self, bot):  # in the class init
+    self.coll = bot.plugin_db.get_partition(self)
 
 class Github(commands.Cog):
 
@@ -11,6 +13,13 @@ class Github(commands.Cog):
         if match:
             issue_num = match.group(1)
             await message.channel.send(f'https://github.com/snaildos/modmail-plugins/issues/{issue_num}')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        match = re.match(r'harddisk#(\d+)', message.content)
+        if match:
+            issue_num = match.group(1)
+            await message.channel.send(f'https://github.com/snaildos/SnailDOS-Hard-Disk-Repair/issues/{issue_num}')
 
 
 def setup(bot):
