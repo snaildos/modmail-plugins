096198b0aed329bde5a7215c119f252b77345638
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 10:13:39 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -5,9 +5,10 @@ from discord.ext import commands
 class Repeater(commands.Cog):
 
 
-    @commands.Cog.listener()
+   @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
         await ctx.send(message)
+
 
 
 def setup(bot):
