55ac71ee223095d9140fda8ffd44b329520e28c4
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 09:49:42 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -9,7 +9,7 @@ class Repeater(commands.Cog):
 
 
 
-    @commands.Cog.listener()
+    @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
         await ctx.send(message)
 
