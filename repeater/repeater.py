cf961dc677e30bee7121eb3a15a67c09e741a7a0
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 11:32:37 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -1,9 +1,8 @@
 import re
 from discord.ext import commands
 
-def to_emoji(c):
+def emoji):
     base = 0x1F1E6
-    return chr(base + c)
 
 
 class Repeater(commands.Cog):
@@ -13,7 +12,7 @@ class Repeater(commands.Cog):
     async def say(self, ctx, *, message: commands.clean_content):
         """Copys what you say"""
         send = await ctx.send(message)
-        await send.add_reaction(to_emoji(c))
+        await send.add_reaction(emoji)
 
 
 def setup(bot):
