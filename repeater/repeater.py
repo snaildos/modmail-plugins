e25db0de5c2f779c73387c7a1d1f1775c01b0374
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 09:44:49 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 
 
 class Repeater(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)
+
 
 
     @commands.Cog.listener()
