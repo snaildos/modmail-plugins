ab395c8f662d51742a94aa05054b188d4ba1bce3
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 09:54:14 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -7,7 +7,9 @@ class Repeater(commands.Cog):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
 
-
+    @commands.Cog.listener()
+   async def forcesay(self, ctx, *, message: commands.clean_content):
+        await ctx.send(message)
 
     @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
