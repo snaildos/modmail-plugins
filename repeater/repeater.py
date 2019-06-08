db8e393cf77dda001a8581bfc7b39a6d9186616d
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 12:01:41 +0800

    Stopped working on emoji

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -1,8 +1,6 @@
 import re
 from discord.ext import commands
 
-def emoji:
-    base = 0x1F1E6
 
 
 class Repeater(commands.Cog):
@@ -11,8 +9,7 @@ class Repeater(commands.Cog):
     @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
         """Copys what you say"""
-        send = await ctx.send(message)
-        await send.add_reaction(emoji)
+        await ctx.send(message)
 
 
 def setup(bot):
