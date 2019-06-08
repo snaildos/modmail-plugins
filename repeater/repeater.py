ce2b2d4fdac453af083a141b5004cb37525adf9f
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 09:36:37 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -1,13 +1,15 @@
-import discord
+import re
 from discord.ext import commands
 
 
-class repeater(commands.Cog):
+class Repeater(commands.Cog):
 
- @commands.command()
-  async def say(self, ctx, *, message: commands.clean_content):
-   await ctx.send(message)
+
+    @commands.command()
+    async def say(self, ctx, *, message: commands.clean_content):
+        await ctx.send(message)
+
 
 
 def setup(bot):
-    bot.add_cog(repeater(bot))
+    bot.add_cog(Github())
