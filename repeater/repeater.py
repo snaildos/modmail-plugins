03dfc450cfba704c37d226efad3586b15e564841
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 10:43:24 +0800

    Update repeater.py

--- repeater/repeater.py
+++ repeater/repeater.py
@@ -5,10 +5,11 @@ from discord.ext import commands
 class Repeater(commands.Cog):
 
 
-   @commands.command()
-async def say(self, ctx, *, message: commands.clean_content):
-        """Follows what you say!!!"""
+    @commands.command()
+    async def say(self, ctx, *, message: commands.clean_content):
+        """Copys what you say"""
         await ctx.send(message)
+
 
 
 
