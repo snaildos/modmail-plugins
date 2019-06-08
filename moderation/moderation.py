4b3de56efb4307596f66bd55471018e78be50121
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-08 17:12:12 +0800

    Create moderation.py

---
+++ moderation/moderation.py
@@ -0,0 +1,27 @@
+import re
+from discord.ext import commands
+
+
+
+class moderator(commands.Cog):
+    def __init__(self, bot):
+        self.bot: discord.Client = bot
+        self.db = bot.plugin_db.get_partition(self)
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def logchannel(self, ctx, channel: discord.TextChannel):
+        """Set up a logchannel for the mod logs.
+        Usage:
+        {prefix}logchannel #channel
+        """
+
+        await self.db.find_one_and_update(
+            {"_id": "config"},
+            {"$set": {"logs": {"channel": str(channel.id)}}},
+            upsert=True,
+        )
+        await ctx.send(f"{channel.mention} is now set up for moderation logs!")
+
+def setup(bot):
+    bot.add_cog(moderator(bot))
