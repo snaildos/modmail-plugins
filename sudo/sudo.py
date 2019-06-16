25526ec922869f9159cfd79080922ff554a038d7
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-16 17:46:14 +0800

    Update sudo.py

--- sudo/sudo.py
+++ sudo/sudo.py
@@ -16,12 +16,12 @@ class sudo(commands.Cog):
        Make user say something.
         """
 
-        webhook = await ctx.channel.create_webhook(name="sudo")
-        await webhook.send(content=msg, username=member.name, avatar_url=member.avatar_url)
+        webhook = await ctx.channel.create_webhook(name="su")
+        await webhook.send(content=msg, username=member.name (sudo), avatar_url=member.avatar_url)
         await webhook.delete()
 
         message = ctx.message
-        message.author = member (sudo)
+        message.author = member
         message.content = msg
         await self.bot.process_commands(message)
 
