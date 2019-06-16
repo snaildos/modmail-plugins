5b042b2cbd76d05f875aed6393fad6dc7f3a0021
Zara-Soonich <46210117+zara-zoonich@users.noreply.github.com>
2019-06-16 16:23:54 +0800

    Create sudo.py

---
+++ sudo/sudo.py
@@ -0,0 +1,27 @@
+import sys
+import discord
+from discord.ext import commands
+
+
+
+class sudo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def sudo(self, ctx, member: discord.Member, *, msg):
+        """
+    Make a user say something
+        """
+
+        webhook = await ctx.channel.create_webhook(name="su")
+        await webhook.send(content=msg, username=member.name, avatar_url=member.avatar_url)
+        await webhook.delete()
+
+        message = ctx.message
+        message.author = member
+        message.content = msg
+        await self.bot.process_commands(message)
+
+
+def setup(bot):
+    bot.add_cog(Reboot(bot))
