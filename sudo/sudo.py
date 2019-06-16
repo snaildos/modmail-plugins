bc8cf6f0d509fa4176adc2f401515cb60755d19f
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-16 11:05:01 +0200

    Update sudo.py

--- sudo/sudo.py
+++ sudo/sudo.py
@@ -2,7 +2,7 @@ from discord.ext import commands as commands
 import discord
 
 
-class sudo(cmd.Cog):
+class sudo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_result = None
