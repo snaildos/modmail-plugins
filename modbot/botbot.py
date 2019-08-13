4ad484f5163d6e96feb281eb39e554e3f65c0636
SnailDOS <46210117+snaildos@users.noreply.github.com>
2019-08-13 20:26:07 +0800

    Add modbot

---
+++ modbot/botbot.py
@@ -0,0 +1,29 @@
+from discord.ext import commands as commands
+from core import checks
+import discord
+from core.models import PermissionLevel
+
+
+class sudo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_result = None
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def modbot(self, ctx, member: discord.Member, *, msg):
+        """
+       Make modbot say somthing.
+        """
+
+        webhook = await ctx.channel.create_webhook(name="su")
+        await webhook.send(content=msg, username=DarkSide-ModBot, avatar_url=https://darksidemc.ga/img/core-img/logo.png)
+        await webhook.delete()
+
+        message = ctx.message
+        message.author = member
+        message.content = msg
+        await self.bot.process_commands(message)
+
+def setup(bot):
+    bot.add_cog(sudo(bot))
